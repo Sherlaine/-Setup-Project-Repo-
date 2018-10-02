a496610632bcd89ac532f161ccf4a68ce6cb8209
Sherlaine <sherlaine.lau@gmail.com>
2018-10-02 03:13:17 +0000

    added avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,29 @@
+var secrets = require ('./secrets');
 var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    
-}
-
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
-  });
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization':'token ' + secrets.GITHUB_TOKEN
+      }
+    };
   
+    request(options, function(err, res, body) {
+      var parsedBody = JSON.parse(body);
+      parsedBody.forEach(function(element){
+        cb(err,element);
+      });
+  
+    });
+  }
 
+
+getRepoContributors("jquery", "jquery", function (err, result) {
+    console.log(result.avatar_url);
+    
+});
